a8e849627e43983e702fcc532d660a64b595be28
deemahalrajhi <117577032+deemahalrajhi@users.noreply.github.com>
2022-11-10 12:51:16 +0300

    Update googleQR.test.js

--- tests/googleQR.test.js
+++ tests/googleQR.test.js
@@ -6,7 +6,7 @@ describe('generateQR()', () => {
         this.url = "www.google.com"; 
         this.color = "11111"; 
         this.height = "3oo";
-        this.width = "300";
+        this.width = "300";%%%%
     });
 
     afterEach(function (){
